37ee822be6ea85e9ceedd90955733e085d36a03c
E1jeong <won994@gmail.com>
2022-06-21 22:19:41 +0900

    node강의 - stream - on chain

--- 12-stream/app-stream.js
+++ 12-stream/app-stream.js
@@ -1,22 +1,18 @@
 const fs = require("fs");
-
-const readStream = fs.createReadStream("./file.txt", {
-  //highWaterMark: 8, // default = 64kb
-  //encoding: "utf-8",
-});
-
 const data = [];
 
-readStream.on("data", (chunk) => {
-  //console.log(chunk);
-  data.push(chunk);
-  console.count("data");
-});
-
-readStream.on("end", () => {
-  console.log(data.join("")); //들어온 문자열 합치기
-});
-
-readStream.on("error", (error) => {
-  console.log(error);
-});
+fs.createReadStream("./file.txt", {
+  //highWaterMark: 8, // default = 64kb
+  //encoding: "utf-8",
+})
+  .on("data", (chunk) => {
+    //console.log(chunk);
+    data.push(chunk);
+    console.count("data");
+  })
+  .on("end", () => {
+    console.log(data.join("")); //들어온 문자열 합치기
+  })
+  .on("error", (error) => {
+    console.log(error);
+  });
